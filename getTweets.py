2db6938a8a2dc806389f325be93d16214dd06952
keziakl <keziakl@stanford.edu>
2020-10-17 01:10:35 -0500

    bug fixes tweepy

--- getTweets.py
+++ getTweets.py
@@ -2,6 +2,11 @@ import requests
 import json
 import csv
 
+#bearer tokens to access Tweets through API
+kez_bearer = "AAAAAAAAAAAAAAAAAAAAAChUGwEAAAAA7ump2ucNmOaE89xv70KhDZqL1qQ%3DVPQSNjNv7BIlk6sO2a0FfU7Rqu75nqFBv08t4KnYk6kavtx2KT"
+wen_bearer = "AAAAAAAAAAAAAAAAAAAAADAzIgEAAAAAa8jC33lX8w0w74HSYSN%2B7jBSmkc%3Delyo4dNtxVtexSCUNBEpbeDSmRv8lXlK1dbcyMEr5OTgvX9FGO"
+
+
 kLimitPerCurseWord = 10000
 kCountPerRequest = 100
 allHashtags = ["sarcasm", "happy", "pissed", "angry", "sad", "annoying", "ugh", "funny", 
@@ -42,9 +47,9 @@ cursewords = set(["asshole", "bitch", "crap", "cunt",
 "blowjob"
 ])
 
-def getTweets(params, curseword, hashtagIndex, writer):
+def getTweets(bearer, params, curseword, hashtagIndex, writer): #deleted headers
 
-  headers = {'Authorization': 'bearer AAAAAAAAAAAAAAAAAAAAADAzIgEAAAAAa8jC33lX8w0w74HSYSN%2B7jBSmkc%3Delyo4dNtxVtexSCUNBEpbeDSmRv8lXlK1dbcyMEr5OTgvX9FGO'}
+  headers = {'Authorization': ('bearer '+bearer)}
   base_url = "https://api.twitter.com/1.1/search/tweets.json"
   uniqueTweets = set()
   while(len(uniqueTweets)<kLimitPerCurseWord):
@@ -63,14 +68,17 @@ def getTweets(params, curseword, hashtagIndex, writer):
     params = res["search_metadata"]["next_results"]
   print("#", allHashtags[hashtagIndex], curseword, "got", len(uniqueTweets), "tweets")
 
-with open('trainng_data_twitter.csv', 'w', newline='') as file:
+
+
+with open('trainng_data_twitter.csv', 'a', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["text", "curseword"]+allHashtags)
 
     for hashtagIndex in range(0, len(allHashtags)):
         for curseword in cursewords:
           try:
-            getTweets("?q=%23" + allHashtags[hashtagIndex] + "%20" + curseword +"&lang=en&tweet_mode=extended&count="+str(kCountPerRequest), curseword, hashtagIndex, writer)
+            getTweets(kez_bearer, "?q=%23" + allHashtags[hashtagIndex] + "%20" + curseword +"&lang=en&tweet_mode=extended&count="+str(kCountPerRequest), curseword, hashtagIndex, writer)
           except Exception as e:
             print("An exception occurred at", hashtagIndex, allHashtags[hashtagIndex], curseword)
             print(e)
+
