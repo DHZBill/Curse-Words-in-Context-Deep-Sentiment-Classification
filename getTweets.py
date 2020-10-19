50c2e9ed2f5dc213a0ab163b1099080946c960af
WenXinDong2018 <wxd@stanford.edu>
2020-10-19 17:40:45 +0800

    format csv file with sentiment label

--- getTweets.py
+++ getTweets.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import csv
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 #bearer tokens to access Tweets through API
 kez_bearer = "AAAAAAAAAAAAAAAAAAAAAChUGwEAAAAA7ump2ucNmOaE89xv70KhDZqL1qQ%3DVPQSNjNv7BIlk6sO2a0FfU7Rqu75nqFBv08t4KnYk6kavtx2KT"
@@ -9,45 +10,38 @@ wen_bearer = "AAAAAAAAAAAAAAAAAAAAADAzIgEAAAAAa8jC33lX8w0w74HSYSN%2B7jBSmkc%3Del
 
 kLimitPerCurseWord = 10000
 kCountPerRequest = 100
-allHashtags = ["sarcasm", "happy", "pissed", "angry", "sad", "annoying", "ugh", "funny", 
-"motivation", "positive", "positivethinking", "anger", "excited", "exciting", "fun", 
-"aggressive", "love", "lol", "hateyou", "loveyou", "pissedoff", "furious", "mad", 
-"afraid", "petrified", "scared","anxious", "worried","joy", "yay","blessed", "thrilled",
-"sadness", "depressed","disappointment", "disappointed", "unhappy","foreveralone", "godie", "pieceofshit", "irritated",
-"fuming", "heated", "angrytweet", "frightened", "freakedout", "haunted", "greatmood", "thankful", "feelinggood", 
-"superhappy", "ecstatic", "atlast", "heartbroken", "leftout"]
+sentiments = {0: "positive", 1:"sad", 2:"angry", 3:"fear", 4:"sarcasm"}
+cursewordsCatg = {
+0: "general",
+1: "race",
+2: "gender-sexuality",
+3:"religion",
+4: "other-body-parts",
+5: "ableist",
+6: "problem-words",
+7:"multiple-worded"}
 
+allCurseWords= [
+  set(["fuck","shit","pissed","screw"]), 
+  set(["nigger","chink","niglet", "wetback"]),
+  set(["dick","cunt","pussy","fag","queer","boner","dong","slut","dyke","pimp","whore","hoe","bitch","cock","tramp","cum","schlong","spunk","skank","motherfucker","tit","gay","mothafucker","screw","blowjob"]),
+  set(["hell","damn"]),
+  set(["ass","queaf","shart","urine","rimming","arse","shat","crap"]),
+  set(["retard","spaz"]),
+  set(["tit","motherfucker","cum","hoe","chink","gay"]),
+  set(["son of a bitch","doggie style","fucked up","ass"])
+]
 
-cursewords = set(["asshole", "bitch", "crap", "cunt", 
-"damn", "fuck", "hell", "shit", "slut", "nigga", "prick", 
-"nigger","dick","retard","tit","son of a bitch",
-"queaf","spaz","motherfucker","doggie style",
-"pissed","niglet","pussy","shart","cum","fucked up",
-"wetback","fag","urine","hoe","ass",
-"queer",
-"rimming",
-"chink",
-"boner","arse",
-"gay",
-"shat",
-"slut",
-"crap",
-"dyke",
-"pimp",
-"whore",
-"hoe",
-"cock",
-"tramp",
-"schlong",
-"spunk",
-"skank",
-"gay",
-"mothafucker",
-"screw",
-"blowjob"
-])
+allHashtags = [
+ set(["happy","funny","greatmood", "superhappy",  "atlast","ecstatic","thankful", "feelinggood", "love","loveyou","joy", "yay","blessed", "thrilled", "lol", "motivation", "positive", "positivethinking", "excited", "exciting", "fun"]),
+  set(["sad","heartbroken", "leftout", "sadness", "depressed","disappointment", "disappointed", "unhappy","foreveralone"]),
+  set(["pissed","angry", "pissedoff", "furious", "mad", "hateyou","annoying", "ugh","anger", "fuming", "heated", "angrytweet","aggressive", "godie", "pieceofshit", "irritated"]),
+  set(["afraid", "petrified", "scared","anxious", "worried", "frightened", "freakedout", "haunted"]),
+  set(["sarcasm"])
+]
 
-def getTweets(bearer, params, curseword, hashtagIndex, writer): #deleted headers
+#helper function
+def getTweets(bearer, params, curseword, hashtag, writer, sentiment, cursewordCatg): #deleted headers
 
   headers = {'Authorization': ('bearer '+bearer)}
   base_url = "https://api.twitter.com/1.1/search/tweets.json"
@@ -55,30 +49,48 @@ def getTweets(bearer, params, curseword, hashtagIndex, writer): #deleted headers
   while(len(uniqueTweets)<kLimitPerCurseWord):
     x = requests.get(base_url+params, headers = headers)
     res = json.loads(x.text)
+    if(not res["statuses"]) :
+      break
     for tweet in res["statuses"]:
       if(tweet["full_text"] not in uniqueTweets):
        # print(tweet["full_text"])
-        row = [0]*len(allHashtags)
-        row[hashtagIndex] = 1
-        writer.writerow([tweet["full_text"], curseword] + row)
+        writer.writerow([tweet["full_text"], sentiment, hashtag, curseword, cursewordCatg])
         uniqueTweets.add(tweet["full_text"])
       
     if("next_results" not in res["search_metadata"]):
       break
     params = res["search_metadata"]["next_results"]
-  print("#", allHashtags[hashtagIndex], curseword, "got", len(uniqueTweets), "tweets")
+  print("#", hashtag, curseword, "got", len(uniqueTweets), "tweets")
 
 
+def runTweetsScraper():
+  with open('well_formatted.csv', 'a', newline='') as file:
+      writer = csv.writer(file)
+      #writer.writerow(["text", "curseword"]+allHashtags)
+      for sentiment, hashtagSet in enumerate(allHashtags):
+        for hashtag in hashtagSet:
+          for cursewordCatg, cursewordSet in enumerate(allCurseWords):
+            for curseword in cursewordSet:
+              try:
+                getTweets(wen_bearer, "?q=%23" + hashtag + "%20" + curseword +"&lang=en&tweet_mode=extended&count="+str(kCountPerRequest), curseword, hashtag, writer, sentiment, cursewordCatg)
+              except Exception as e:
+                print("An exception occurred at", hashtag, curseword)
+                print(e)
 
-with open('trainng_data_twitter.csv', 'a', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["text", "curseword"]+allHashtags)
+def findNumUniqueTweets():
+    twitter = pd.read_csv('./well_formatted.csv')
+    print("all", twitter.shape)
+    firstrow = 0
+    uniqueSet = set()
+    for row in twitter.iterrows():
+      if(firstrow == 0) :
+        firstrow == row
+        continue
+      text = row[1][0]
+      uniqueSet.add(text)
+    print("unique ones", len(uniqueSet))
 
-    for hashtagIndex in range(0, len(allHashtags)):
-        for curseword in cursewords:
-          try:
-            getTweets(kez_bearer, "?q=%23" + allHashtags[hashtagIndex] + "%20" + curseword +"&lang=en&tweet_mode=extended&count="+str(kCountPerRequest), curseword, hashtagIndex, writer)
-          except Exception as e:
-            print("An exception occurred at", hashtagIndex, allHashtags[hashtagIndex], curseword)
-            print(e)
+
+#runTweetsScraper()
+
 
